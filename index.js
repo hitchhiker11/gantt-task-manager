7d63acff8233ec59f0e0e9a510a05ceca588eaac
cheerfullhitchhiker <96291048+cheerfullhitchhiker@users.noreply.github.com>
2022-08-15 17:19:55 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App.js";
+
+import themes from "devextreme/ui/themes";
+themes.initialized(() =>
+  ReactDOM.render(<App />, document.getElementById("app"))
+);
